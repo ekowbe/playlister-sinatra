94ca5534c2618c89dd4866ff7face43c9ba6a3bc
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-30 19:07:16 +0000

    stuff

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,13 @@
 class ArtistsController < ApplicationController
     # index, new, show and edit routes
 
-    get '/users' do
-      erb :"users/index"
+    get '/artist' do
+        @artist =Artist.all
+      erb :"artists/index"
+    end
+    
+    get '/artists/slug' do
+        
+        erb :'artists/show'
     end
 end
