2c502bdba02d50dba520a92b5f05a59a13c35857
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-30 18:29:50 +0000

    prelim stuff

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,7 @@
 class ArtistsController < ApplicationController
+    # index, new, show and edit routes
+
+    get '/users' do
+      erb :"users/index"
+    end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -19,5 +19,5 @@ ActiveRecord::Base.establish_connection(
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require_all 'app'
-require_all 'lib'
+require_all '../app'
+require_all '../lib'
